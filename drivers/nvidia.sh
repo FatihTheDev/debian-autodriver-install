a678b171cd4e55d7abbd041500f32a49a164312a
FatihTheDev <fatihtheg123@gmail.com>
2025-08-03 15:47:06 +0200

    Updated nvidia.sh

--- drivers/nvidia.sh
+++ drivers/nvidia.sh
@@ -6,7 +6,9 @@ echo "Installing NVIDIA proprietary drivers from $CODENAME-backports..."
 sudo apt install -y -t $CODENAME-backports \
     nvidia-driver \
     nvidia-settings \
-    nvidia-vdpau-driver
+    nvidia-vdpau-driver \
+    libnvcuvid1 \
+    libnvidia-encode1 
 
 # Check for Intel iGPU (Optimus detection)
 if lspci | grep -i "vga" | grep -qi "intel"; then
